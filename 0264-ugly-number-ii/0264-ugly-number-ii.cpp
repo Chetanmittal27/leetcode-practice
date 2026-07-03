d410068173bc7985c355a70a890ce04e4e847b9f
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-07-03 16:46:50 +0530

    Time: 3 ms (79.58%), Space: 11.3 MB (85.02%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,29 +1,68 @@
+// class Solution {
+// public:
+
+//     int nthUglyNumber(int n) {
+
+//         vector<long long>ans;
+
+//         priority_queue<long long , vector<long long> , greater<long long>>q;
+
+//         q.push(1);
+
+//         while(n > 0 && !q.empty()){
+
+//             long long val = q.top();
+//             q.pop();
+
+//             if(ans.size() == 0 || ans.back() != val){
+//                 ans.push_back(val);
+//                 n--;
+
+//                 q.push(val * 2);
+//                 q.push(val * 3);
+//                 q.push(val * 5);
+//             }
+//         }
+
+//         return ans.back();
+//     }
+// };
+
+
+
 class Solution {
 public:
 
     int nthUglyNumber(int n) {
 
-        vector<long long>ans;
+        vector<int>ans(n+1);
 
-        priority_queue<long long , vector<long long> , greater<long long>>q;
+        ans[1] = 1;
 
-        q.push(1);
+        int i2 = 1 , i3 = 1 , i5 = 1;
 
-        while(n > 0 && !q.empty()){
+        for(int i = 2; i <= n; i++){
 
-            long long val = q.top();
-            q.pop();
+            int val2 = ans[i2] * 2;
+            int val3 = ans[i3] * 3;
+            int val5 = ans[i5] * 5;
 
-            if(ans.size() == 0 || ans.back() != val){
-                ans.push_back(val);
-                n--;
+            int mini = min(val2 , min(val3 , val5));
+            ans[i] = mini;
+            
+            if(mini == val2){
+                i2++;
+            }
 
-                q.push(val * 2);
-                q.push(val * 3);
-                q.push(val * 5);
+            if(mini == val3){
+                i3++;
+            }
+
+            if(mini == val5){
+                i5++;
             }
         }
 
-        return ans.back();
+        return ans[n];
     }
 };
