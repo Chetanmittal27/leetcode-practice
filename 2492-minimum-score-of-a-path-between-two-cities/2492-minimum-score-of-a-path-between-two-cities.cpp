d6069a0ec10b70b2c7634dff519ab1a4dc291074
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-07-04 12:51:04 +0530

    Time: 107 ms (26.77%), Space: 146.1 MB (15.63%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,5 +1,76 @@
+// class Solution {
+// public:
+//     int minScore(int n, vector<vector<int>>& roads) {
+        
+//         vector<vector<pair<int,int>>>adj(n+1);
+
+//         for(auto road : roads){
+//             int u = road[0];
+//             int v = road[1];
+//             int w = road[2];
+
+//             adj[u].push_back({v , w});
+//             adj[v].push_back({u , w});
+//         }
+
+
+//         priority_queue<tuple<int,int,int> , vector<tuple<int,int,int>> , greater<tuple<int,int,int>>>q;
+
+//         q.push({0 , -1 , 1});
+
+//         int mini = INT_MAX;
+        
+//         vector<int>vis(n+1 , 0);
+
+//         while(!q.empty()){
+
+//             auto [wt , u , v] = q.top();
+//             q.pop();
+
+//             if(vis[v]) continue;
+
+//             vis[v] = 1;
+
+//             if(u != -1){
+//                 mini = min(mini , wt);
+//             }
+
+//             for(auto it : adj[v]){
+//                 int adjCity = it.first;
+//                 int adjWt = it.second;
+
+//                 if(!vis[adjCity]){
+//                     q.push({adjWt , v , adjCity});
+//                 }
+//             }
+//         }
+
+//         return mini;
+//     }
+// };
+
+
+
 class Solution {
 public:
+
+    void dfs(vector<vector<pair<int,int>>>& adj , vector<int>& vis , int node , int& mini){
+        
+        vis[node] = 1;
+
+        for(auto it : adj[node]){
+            int v = it.first;
+            int wt = it.second;
+
+            mini = min(mini , wt);
+
+            if(!vis[v]){
+                dfs(adj , vis , v , mini);
+            }
+        }
+    }
+
+
     int minScore(int n, vector<vector<int>>& roads) {
         
         vector<vector<pair<int,int>>>adj(n+1);
@@ -14,36 +85,11 @@ public:
         }
 
 
-        priority_queue<tuple<int,int,int> , vector<tuple<int,int,int>> , greater<tuple<int,int,int>>>q;
-
-        q.push({0 , -1 , 1});
-
-        int mini = INT_MAX;
-        
         vector<int>vis(n+1 , 0);
 
-        while(!q.empty()){
+        int mini = INT_MAX;
 
-            auto [wt , u , v] = q.top();
-            q.pop();
-
-            if(vis[v]) continue;
-
-            vis[v] = 1;
-
-            if(u != -1){
-                mini = min(mini , wt);
-            }
-
-            for(auto it : adj[v]){
-                int adjCity = it.first;
-                int adjWt = it.second;
-
-                if(!vis[adjCity]){
-                    q.push({adjWt , v , adjCity});
-                }
-            }
-        }
+        dfs(adj , vis , 1 , mini);
 
         return mini;
     }
