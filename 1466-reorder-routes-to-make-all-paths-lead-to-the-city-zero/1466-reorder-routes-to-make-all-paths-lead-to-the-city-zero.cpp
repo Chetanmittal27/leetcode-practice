289039081d6adb632213601cac8fc8efa0ae6b87
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-07-28 13:32:48 +0530

    Time: 67 ms (99.68%), Space: 107.5 MB (56.62%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    void dfs(vector<vector<int>>& connections , vector<vector<pair<int,int>>>& adj , vector<int>& vis , int node , int &count){
+
+        vis[node] = 1;
+
+        for(auto it : adj[node]){
+            int v = it.first;
+            int type = it.second;
+
+            if(!vis[v]){
+                count = count + type;
+                dfs(connections , adj , vis , v , count);
+            }
+        }
+    }
+
+
+    int minReorder(int n, vector<vector<int>>& connections) {
+        
+        vector<vector<pair<int,int>>>adj(n);
+
+        for(auto &edge : connections){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back({v , 1});
+            adj[v].push_back({u , 0});
+        }
+
+
+        vector<int>vis(n , 0);
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+
+            if(!vis[i]){
+                dfs(connections , adj , vis , i , count);
+            }
+        }
+
+        return count;
+    }
+};
