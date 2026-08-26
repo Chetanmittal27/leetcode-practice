8dcaefe08f543c705456369fcef56db9720993d9
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-08-26 14:29:08 +0530

    Time: 4 ms (14.09%), Space: 10.3 MB (22.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,15 +5,18 @@ public:
         string str = "";
 
         for(auto ch : s){
+
             if(isalnum(ch)){
-                str += static_cast<char>(tolower(ch));
+                str.push_back((char)tolower((unsigned char)ch));
             }
         }
 
         int n = str.length();
-        int left = 0 , right = n-1;
+
+        int left = 0 , right = n - 1;
 
         while(left < right){
+
             if(str[left] != str[right]){
                 return false;
             }
