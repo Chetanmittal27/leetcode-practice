70095dafe7a3b09c8d8bf0485d4a57ba8dde64a4
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-08-28 17:24:27 +0530

    Time: 17 ms (26.25%), Space: 24.6 MB (41.77%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
 
-    void dfs(vector<vector<int>>& grid , vector<vector<int>>& vis , int start , int end , queue<pair<int,int>>& q){
+    void dfs(vector<vector<int>>& grid , vector<vector<int>>& vis , queue<tuple<int,int,int>>& q , int row , int col){
 
-        vis[start][end] = 1;
-        q.push({start , end});
+        vis[row][col] = 1;
+        q.push({row , col , 0});
 
         int dx[4] = {0 , -1 , 0 , +1};
         int dy[4] = {-1 , 0 , +1 , 0};
 
         for(int i = 0; i < 4; i++){
 
-            int r = start + dx[i];
-            int c = end + dy[i];
+            int r = row + dx[i];
+            int c = col + dy[i];
 
-            if((r >= 0 && r < grid.size()) && (c >= 0 && c < grid[0].size()) && grid[r][c] == 1){
+            if((r >= 0 && r < grid.size()) && (c >= 0 && c < grid.size()) && grid[r][c] == 1){
 
                 if(!vis[r][c]){
-                    dfs(grid , vis , r , c , q);
+
+                    dfs(grid , vis , q , r , c);
                 }
             }
         }
@@ -28,69 +29,59 @@ public:
         
         int n = grid.size();
 
-        vector<vector<int>>vis(n , vector<int>(n , 0));
-
-        queue<pair<int , int>>q;
-
-        int start = -1 , end = -1;
+        int startRow = -1 , startCol = -1;
 
         for(int i = 0; i < n; i++){
+
             for(int j = 0; j < n; j++){
 
                 if(grid[i][j] == 1){
-                    start = i;
-                    end = j;
+
+                    startRow = i;
+                    startCol = j;
+                    break;
+                }
+
+                if(startRow != -1 && startCol != -1){
                     break;
                 }
             }
-
-            if(start != -1 && end != -1){
-                break;
-            }
         }
 
+        vector<vector<int>>vis(n , vector<int>(n , 0));
 
-        dfs(grid , vis , start , end , q);
+        queue<tuple<int,int,int>>q;
+
+        dfs(grid , vis , q , startRow , startCol);
 
         int dx[4] = {0 , -1 , 0 , +1};
         int dy[4] = {-1 , 0 , +1 , 0};
 
-        int ans = 0;
-
         while(!q.empty()){
 
-            int len = q.size();
+            auto [row , col , steps] = q.front();
+            q.pop();
 
-            ans++;
+            for(int i = 0; i < 4; i++){
 
-            for(int i = 0; i < len; i++){
+                int r = row + dx[i];
+                int c = col + dy[i];
 
-                auto p = q.front();
-                q.pop();
+                if((r >= 0 && r < n) && (c >= 0 && c < n)){
 
-                int row = p.first;
-                int col = p.second;
+                    if(grid[r][c] == 1 && !vis[r][c]){
+                        return steps;
+                    }
 
-                for(int j = 0; j < 4; j++){
-
-                    int r = row + dx[j];
-                    int c = col + dy[j];
-
-                    if((r >= 0 && r < n) && (c >= 0 && c < n)){
-
-                        if(grid[r][c] == 1 && !vis[r][c]){
-                            return ans-1;
-                        }
-
-                        if(grid[r][c] == 0 && !vis[r][c]){
-                            vis[r][c] = 1;
-                            q.push({r , c});
-                        }
+                    if(grid[r][c] == 0 && !vis[r][c]){
+                        grid[r][c] = 1;
+                        vis[r][c] = 1;
+                        q.push({r , c , steps + 1});
                     }
                 }
             }
         }
 
-        return ans-1;
+        return 0;
     }
 };
