d61e9c9c368aac9bc1148524be1277490976f909
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-09-05 02:10:33 +0530

    Time: 977 ms (78.54%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+with cte as (
+    select r.contest_id , round(count(u.user_id) * 100.0 / (select count(*) from users) , 2) as percentage from Register r
+    left join
+    Users u on r.user_id = u.user_id
+    group by r.contest_id
+)
+
+select contest_id , percentage from cte
+order by percentage desc , contest_id asc;
