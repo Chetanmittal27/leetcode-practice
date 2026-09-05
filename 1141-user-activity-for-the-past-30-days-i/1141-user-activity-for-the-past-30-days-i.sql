25f68a7b74ae9d95e56b4750a3521e24714dabea
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-09-05 23:02:04 +0530

    Time: 461 ms (94.87%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,3 @@
+select activity_date as day , count(distinct(user_id)) as active_users from Activity
+group by activity_date
+having activity_date > DATE_SUB('2019-07-27', INTERVAL 30 DAY) and activity_date <= '2019-07-27';
