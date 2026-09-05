1852eef9d2055ce428e3d4be62a95659cbb13261
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-09-06 00:01:44 +0530

    Time: 1931 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+with cte as (
+    select product_id , year , quantity , price , dense_rank() over(partition by product_id order by year) as rn 
+    from Sales
+)
+
+select product_id , year as first_year , quantity , price from cte
+where rn = 1;
