7af60a2aac48cac92d3b6c6138044becde824973
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-09-11 22:42:24 +0530

    Time: 10 ms (5.26%), Space: 21 MB (12.89%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -17,24 +17,24 @@ public:
         
         int n = isConnected.size();
 
-        vector<vector<int>>adj(n+1);
+        vector<vector<int>>adj(n);
 
         for(int i = 0; i < n; i++){
 
             for(int j = 0; j < n; j++){
 
-                if(i != j  &&  isConnected[i][j] == 1){
-
-                    adj[i+1].push_back(j+1);
+                if(i != j && isConnected[i][j] == 1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
 
-
-        vector<int>vis(n+1 , 0);
+        vector<int>vis(n , 0);
         int count = 0;
 
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
+
             if(!vis[i]){
                 count++;
                 dfs(adj , vis , i);
