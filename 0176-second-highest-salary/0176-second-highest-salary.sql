8b6b0456a9f9dd17a9b75758f99a10f3a67ab0c1
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-09-18 03:05:31 +0530

    Time: 329 ms (31.79%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+select coalesce(
+    (select distinct(salary) from Employee
+    where salary < (select max(salary) from Employee)
+    order by salary desc
+    limit 1),
+    
+    null
+) as SecondHighestSalary;
