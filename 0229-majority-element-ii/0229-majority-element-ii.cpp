22878af8946ec8ed1277934c699f8686bebc2ed9
Chetanmittal27 <153277496+Chetanmittal27@users.noreply.github.com>
2026-09-25 23:54:02 +0530

    Time: 0 ms (100%), Space: 23.7 MB (76.74%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,22 +4,49 @@ public:
         
         int n = nums.size();
 
-        unordered_map<int , int>mpp;
+        int count1 = 0 , ele1 = -1;
+        int count2 = 0 , ele2 = -1;
 
         for(int i = 0; i < n; i++){
 
-            mpp[nums[i]]++;
+            if(count1 == 0 && nums[i] != ele2){
+                ele1 = nums[i];
+                count1 = 1;
+            }
+
+            else if(count2 == 0 && nums[i] != ele1){
+                ele2 = nums[i];
+                count2 = 1;
+            }
+
+            else if(nums[i] == ele1){
+                count1++;
+            }
+
+            else if(nums[i] == ele2){
+                count2++;
+            }
+
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+
+        int cnt1 = 0 , cnt2 = 0;
+
+        for(int i = 0; i < n; i++){
+
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;
         }
 
         vector<int>ans;
 
-        for(auto it : mpp){
+        if(cnt1 > n/3) ans.push_back(ele1);
+        if(cnt2 > n/3 && ele2 != ele1) ans.push_back(ele2);
 
-            if(it.second > n/3){
-
-                ans.push_back(it.first);
-            }
-        }
 
         return ans;
     }
